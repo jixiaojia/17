d010698790ccd18c76c90dea04604bd2f0a4dc62
jixiaojia <1635511678@qq.com>
2018-01-02 17:10:48 +0800

    高大上的操作

---
+++ src/haha/ji.java
@@ -0,0 +1,20 @@
+package haha;
+
+
+import java.util.Scanner;
+
+public class ji {
+
+   public static void main(String[] args){
+    Scanner sc=new  Scanner(System.in);
+   System.out.println("输入一个数");
+   int b=sc.nextInt();
+    shuchu(b);
+}
+   public static void shuchu(int a){
+	   
+	         for(int i=1;i<=a;i++){
+	        	 System.out.println(i);
+	        	 }
+	         }
+   }
